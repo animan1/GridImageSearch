85cc0f32264f67bf7acc7224ea04c4b8bcfc2b2f
Daniel Boeve <dboeve@hearsaycorp.com>
2015-05-11 18:34:50 -0700

    Empty Android Studio project.

---
+++ app/src/main/java/com/codepath/gridimagesearch/activities/StreamActivity.java
@@ -0,0 +1,17 @@
+package com.codepath.gridimagesearch.activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.codepath.gridimagesearch.R;
+
+
+public class StreamActivity extends ActionBarActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_stream);
+  }
+}
